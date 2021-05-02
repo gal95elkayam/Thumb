d9a2d9ea78086ff8d0969c033956cc6cc54af7e9
Gal <gal95elkayam@gmail.com>
2021-05-02 11:17:39 +0300

    sms and location

---
+++ app/src/main/java/com/example/thumb/PermissionActivity.java
@@ -0,0 +1,133 @@
+package com.example.thumb;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class PermissionActivity extends AppCompatActivity {
+    private Context mContext;
+    private Activity mActivity;
+
+    private LinearLayout mRootLayout;
+
+    private static final int MY_PERMISSIONS_REQUEST_CODE = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get the application context
+        mContext = getApplicationContext();
+        mActivity = PermissionActivity.this;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkPermission();
+        }
+    }
+
+
+    protected void checkPermission(){
+        if(ContextCompat.checkSelfPermission(mActivity, Manifest.permission.SEND_SMS)
+                + ContextCompat.checkSelfPermission(
+                mActivity,Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+
+            // Do something, when permissions not granted
+            if(ActivityCompat.shouldShowRequestPermissionRationale(
+                    mActivity,Manifest.permission.SEND_SMS)
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                    mActivity,Manifest.permission.ACCESS_FINE_LOCATION)){
+                // If we should give explanation of requested permissions
+                // Show an alert dialog here with request explanation
+                AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+                builder.setMessage("Location and Send Sms External" +
+                        " Storage permissions are required to do the task.");
+                builder.setTitle("Please grant those permissions");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ActivityCompat.requestPermissions(
+                                mActivity,
+                                new String[]{
+                                        Manifest.permission.SEND_SMS,
+                                        Manifest.permission.ACCESS_FINE_LOCATION
+                                },
+                                MY_PERMISSIONS_REQUEST_CODE
+                        );
+                    }
+                });
+                builder.setNeutralButton("Cancel",null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }else{
+                // Directly request for required permissions, without explanation
+                ActivityCompat.requestPermissions(
+                        mActivity,
+                        new String[]{
+                                Manifest.permission.SEND_SMS,
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                        },
+                        MY_PERMISSIONS_REQUEST_CODE
+                );
+            }
+        }else {
+            // Do something, when permissions are already granted
+            Toast.makeText(mContext,"Permissions already granted",Toast.LENGTH_SHORT).show();
+            Intent intent=new Intent(PermissionActivity.this, ShakeEmergencyActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_CODE:{
+                // When request is cancelled, the results array are empty
+                if(
+                        (grantResults.length >0) &&
+                                (grantResults[0]
+                                        + grantResults[1]
+                                        + grantResults[2]
+                                        == PackageManager.PERMISSION_GRANTED
+                                )
+                ){
+                    // Permissions are granted
+                    Toast.makeText(mContext,"Permissions granted.",Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(PermissionActivity.this, ShakeEmergencyActivity.class);
+                    startActivity(intent);
+                }else {
+                    // Permissions are denied
+                    Toast.makeText(mContext,"Permissions denied.",Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/thumb/RegisterActivityGoogel.java
@@ -0,0 +1,70 @@
+package com.example.thumb;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class RegisterActivityGoogel extends AppCompatActivity {
+
+    Button btnRegisterClient;
+    Button btnRegisterVoulenteer;
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_for_google);
+
+        btnRegisterClient=findViewById(R.id.buttonClient);
+        btnRegisterVoulenteer= findViewById(R.id.buttonVoulenteer);
+        final Intent[] intent = new Intent[1];
+
+        btnRegisterClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent[0] =new Intent(RegisterActivityGoogel.this, ClientActivity.class);
+                startActivity(intent[0]);
+                finish();
+            }
+        });
+
+        btnRegisterVoulenteer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                intent[0] =new Intent(RegisterActivityGoogel.this, VolunteerActivity.class);
+                startActivity(intent[0]);
+                finish();
+
+            }
+        });
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
